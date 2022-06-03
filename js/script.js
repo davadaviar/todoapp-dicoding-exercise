c87fe20e75c59b0ceb607b29c2a95a18aeef3445
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-06-03 22:50:10 +0700

    Add function makeTodo

--- js/script.js
+++ js/script.js
@@ -36,3 +36,22 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
 document.addEventListener(RENDER_EVENT, function() {
     console.log(todos);
 })
+
+function makeTodo(todoObject) {
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = todoObject.task;
+
+    const textTimestamp = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textTimestamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+    container.setAttribute('id', `todo-${todoObject.id}`);
+
+    return container;
+}
